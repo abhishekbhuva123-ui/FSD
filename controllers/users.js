565c632290506ddc7c56d57b4f0386f8ed329b0f
abhishekbhuva123-ui <abhishekbhuva123@gmail.com>
2026-04-27 16:20:37 +0530

    ERR

--- controllers/users.js
+++ controllers/users.js
@@ -20,7 +20,7 @@ module.exports.signup = async (req, res, next) => {
       }
     });
   } catch (e) {
-    console.log(err);
+    console.log(e);
     req.flash("error", e.message);
     res.redirect("/signup");
   }
